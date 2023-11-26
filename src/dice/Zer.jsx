bab5f451a59db58a0ed20863a15e2d6c4c1f83d0
Gunel68 <gpenahi806@gmail.com>
2023-11-26 12:18:55 +0400

    Zer

---
+++ src/dice/Zer.jsx
@@ -0,0 +1,57 @@
+// import React from 'react'
+// import img1 from './imgs/img1.png'
+// import img2 from './imgs/img2.png'
+// const Zer = () => {
+//   return (
+//     <div>
+//       <img src={img1} alt='img1'/>
+//       <img src={img2} alt='img2'/>
+
+//     </div>
+//   )
+// }
+
+// export default Zer
+import React, { useState } from 'react';
+import img1 from './imgs/img1.png';
+import img2 from './imgs/img2.png';
+import img3 from './imgs/img3.png';
+import img4 from './imgs/img4.png';
+import img5 from './imgs/img5.png';
+import img6 from './imgs/img6.png';
+const Zer = () => {
+  const [selectedDama1, setSelectedDama1] = useState(1);
+  const [selectedDama2, setSelectedDama2] = useState(2);
+
+  const handleDamaClick = (damaNumber) => {
+    const randomDama = Math.floor(Math.random() * 6) + 1; // Random number between 1 and 6
+
+    if (damaNumber === 1) {
+      setSelectedDama1(randomDama);
+    } else if (damaNumber === 2) {
+      setSelectedDama2(randomDama);
+    }
+  };
+
+  const selectedDamaSrc = {
+    1: [img1, img2, img3, img4, img5, img6],
+    2: [img1, img2, img3, img4, img5, img6],
+  };
+
+  return (
+    <div>
+      <img
+        src={selectedDamaSrc[1][selectedDama1 - 1]}
+        alt={`img${selectedDama1}`}
+        onClick={() => handleDamaClick(1)}
+      />
+      <img
+        src={selectedDamaSrc[2][selectedDama2 - 1]}
+        alt={`img${selectedDama2}`}
+        onClick={() => handleDamaClick(2)}
+      />
+    </div>
+  );
+};
+
+export default Zer;
